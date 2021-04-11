d8489d4d07c2d3e65dce35052307b09d03b69345
Sean Fallon <liquidthoughts333@gmail.com>
2021-04-10 19:50:54 -0700

    Art parallex

---
+++ js projects/liquidlogo.js
@@ -0,0 +1,19 @@
+let liqlogo = document.getElementById('liqlogo');
+
+let step_num = 1;
+
+let liqLogoAnimate = function(){
+
+    if(step_num > 26){
+
+        step_num = 1;
+    }
+
+
+    liqlogo.src = '/js projects/small-projects/liquid_logo_animation/logo_frame_'+step_num+'.png';
+
+    step_num = step_num + 1;
+
+    setTimeout(liqLogoAnimate,100);
+
+}
